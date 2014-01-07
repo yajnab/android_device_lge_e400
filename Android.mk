4fb56a6e29873b4c512a5b2104d2e6a7c4fbefd7
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2014-01-07 13:06:30 +0530

    e400: Hack for Libcamera Compilation
    This is hacky, Will be fixed soon

--- Android.mk
+++ Android.mk
@@ -1,3 +1,6 @@
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),e400)
 include $(call first-makefiles-under,$(call my-dir))
 endif
+# HACK for prebuilt libcamera
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/export_includes)
